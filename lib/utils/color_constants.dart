6de316ae2ff5f5d12a1e9352425f23b9916fdd15
uemirhanselim <uemirhanselim@gmail.com>
2022-10-14 10:10:33 +0300

    Color constants created

---
+++ lib/utils/color_constants.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class ColorConstants {
+  static const Color blueRuin = Color.fromRGBO(0, 112, 218, 0);
+  static const Color adirondackBlue = Color.fromRGBO(117, 134, 145, 0);
+  static const Color freinachtBlack = Color.fromRGBO(32, 45, 52, 0);
+  static const Color white = Color.fromRGBO(255, 255, 255, 0);
+  static const Color darkRift = Color.fromRGBO(5, 14, 18, 0);
+  static const Color blackIsBack = Color.fromRGBO(10, 23, 30,0);
+}
